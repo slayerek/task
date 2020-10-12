5a8aca76db60cd5cc6eeb58a95c2b24a1f40b96c
Maciej Zaziablo <maciej.zaziablo@precisely.com>
2020-10-12 15:57:47 +0200

    working on product

--- task/src/models/product.model.ts
+++ task/src/models/product.model.ts
@@ -8,4 +8,15 @@ export class Product implements ProductInterface {
     image: string;
     promo: boolean;
     active: boolean;
+
+    constructor(id: number, name: string, description: string, rating: number, image: string, promo: boolean, active: boolean) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.rating = rating;
+        this.image = image;
+        this.promo = promo;
+        this.active = active;
+    }
+
 }
--- task/src/services/product.service.ts
+++ task/src/services/product.service.ts
@@ -1,16 +1,41 @@
 import {Injectable} from '@angular/core';
 import {Product} from '../models/product.model';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {Router} from "@angular/router";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductService {
 
-    constructor() {}
+    private apiUrl = 'https://join-tsh-api-staging.herokuapp.com/product'
 
-    public getProduct() {
+    constructor(private http: HttpClient, private router: Router) {}
 
-        const apiUrl = 'https://join-tsh-api-staging.herokuapp.com/product';
+    public getProduct(): Observable<Object> {
+
+        return this.http.get<Product[]>(this.apiUrl).pipe(
+            map(
+                res => {
+                    return res['items'].map(
+                        item => {
+                            return new Product(
+                                item['id'],
+                                item['name'],
+                                item['description'],
+                                item['rating'],
+                                item['image'],
+                                item['promo'],
+                                item['active']
+                            );
+                        }
+                    )
+
+                }
+            )
+        );
 
     }
 }
